909ff613bec197f59a85e68b6c65eef790de3cbb
suman dumre <sumandumre90@gmail.com>
2026-05-23 12:46:10 +0545

    backend update

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,21 @@ require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 
+const bodyParser= require('body-parser');
+const cors= require('cors');
+
 const {HoldingsModel}=require('./model/HoldingsModel');
 const {PositionsModel}=require('./model/PositionsModel');
+const {OrdersModel}= require('./model/OrdersModel');
 const PORT = process.env.PORT || 3002;
 const url = process.env.MONGO_URL;
 
+
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
 
 app.get('/addHoldings',async(req,res)=>{
 let tempHoldings=[
@@ -189,6 +198,18 @@ let allPositions= await PositionsModel.find({});
 res.json(allPositions);
 });
 
+app.post('/newOrder', async(req,res)=>{
+let newOrder = new OrdersMOdel({
+   name: req.body.name,
+    qty: req.body.qty,
+    price: req.body.price,
+    mode:req.body.mode,
+});
+
+newOrder.save();
+res.send("Order Saved!");
+});
+
 
 app.listen(PORT, async () => {
     console.log("App Started");
@@ -201,3 +222,5 @@ app.listen(PORT, async () => {
         console.log(error.message);
     }
 });
+
+
--- backend/schemas/OrdersSchema.js
+++ backend/schemas/OrdersSchema.js
@@ -3,7 +3,7 @@ const {Schema}=require("mongoose");
 const OrdersSchema=new Schema({
  name: String,
     qty: Number,
-    avg: Number,
+  
     price: Number,
     mode:String,
 });
