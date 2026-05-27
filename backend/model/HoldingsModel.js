5cb4a41729ea95f1611307a44ec9e55575785b8c
suman dumre <sumandumre90@gmail.com>
2026-05-27 16:17:41 +0545

    final update

--- backend/model/HoldingsModel.js
+++ backend/model/HoldingsModel.js
@@ -3,4 +3,6 @@ const {model}=require("mongoose");
 const {HoldingsSchema}=require('../schemas/HoldingsSchema');
 
 const HoldingsModel=new model("holding",HoldingsSchema);
+
+
 module.exports={HoldingsModel};
